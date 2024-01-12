772241e2b3cc32147caba454cd2fb59ef1c9a7b7
Valentina Tellez <tellez.laura321@gmail.com>
2024-01-11 19:11:34 -0500

    Creacion del componente Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,8 @@
+export function Formulario(props) {
+  return (
+    <form>
+      <input type="text" placeholder="Introducce la Tarea" />
+      <input type="submit" className="btn" value="AGREGAR" />
+    </form>
+  );
+}
