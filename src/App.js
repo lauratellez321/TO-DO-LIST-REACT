ddc5a70fb7591e8a49ba213f1a41e9b935075b9d
Valentina Tellez <tellez.laura321@gmail.com>
2024-01-12 14:55:18 -0500

    Implementar funcionalidad para agregar nuevas tareas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 import { Formulario } from "./components/Formulario";
 
 function App() {
+  const [tarea, setTarea] = useState("");
+  const [listadoTareas, setListadoTareas] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (tarea === "") {
+      alert("DEBES INGRESAR UNA TAREA");
+      return;
+    }
+
+    const nuevaTarea = {
+      id: Date.now(),
+      tarea: tarea,
+      completado: false,
+    };
+    const temp = [nuevaTarea, ...listadoTareas];
+    setListadoTareas(temp);
+
+    setTarea("");
+
+    console.log(listadoTareas);
+  }
+
+  function handleChange(e) {
+    setTarea(e.target.value);
+    console.log(tarea);
+  }
   return (
     <>
       <div className="contenedorPrincipal">
         <h1>TO-DO LIST</h1>
 
         <div className="contenedorFormulario">
-          <Formulario />
+          <Formulario
+            tarea={tarea}
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+          />
         </div>
       </div>
     </>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,19 @@
 export function Formulario(props) {
+  const { tarea, handleSubmit, handleChange } = props;
   return (
-    <form>
-      <input type="text" placeholder="Introducce la Tarea" />
-      <input type="submit" className="btn" value="AGREGAR" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Introducce la Tarea"
+        onChange={handleChange}
+        value={tarea}
+      />
+      <input
+        type="submit"
+        className="btn"
+        value="AGREGAR"
+        onClick={handleSubmit}
+      />
     </form>
   );
 }
